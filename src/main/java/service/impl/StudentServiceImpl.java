fa21e4ed57e32295a87cb98ac58a8856125ca1e9
bala-amypo <bala@amypo.in>
2025-12-18 05:35:33 +0000

    User commit

--- src/main/java/service/impl/StudentServiceImpl.java
+++ src/main/java/service/impl/StudentServiceImpl.java
@@ -1,32 +1,38 @@
 package com.example.demo.service.impl;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.stereotype.Service;
+import java.util.*;
 import com.example.demo.entity.Student;
 import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.StudentService;
-import java.util.*;
- 
+
 @Service
 public class StudentServiceImpl implements StudentService{
     @Autowired
     StudentRepository stdrepo;
-    @Override
-    public Student poststudent(Student st){
-        return stdrepo.save (st);
-    @Override
-    public List<Student> getAllstudent(){
-        return stdrepo.findALL ();
+  @Override
+   public Student poststudent(Student st){
+       return stdrepo.save(st);
+   } 
+   @Override
+   public List<Student> getAllStudent(){
+    return stdrepo.findAll();
+   }
+   @Override
+   public Optional<Student> getById(Long id){
+    return stdrepo.findById(id);
+   }
+   @Override
+   public String updateData(Long id,Student st){
+    boolean status=stdrepo.existsById(id);
+    if(status){
+        st.setId(id);
+        stdrepo.save(st);
+        return "Student updates Successfully";
     }
-    @Override
-    public Optional<Student>getById(Long id){
-        return stdrepo.findById(id);
-    }
-    @Override
-    public String updateDate(Long id,Student st){
-        boolean status=stdrepo.existsById(id);
-        if(status){
-            st.setId(id);
-            stdrepo.save(st);
-        }
-    }
+    else{
+        return "Student with Id"+id+" not found";
+    }
+}
 }
